5248dced8bbd20241343f9fef1036d77b9269732
Ales Nosek <anosek@redhat.com>
2019-03-30 14:34:20 -0700

    Add definition of common constants

---
+++ src/main/java/com/redhat/photogallery/common/Constants.java
@@ -0,0 +1,10 @@
+package com.redhat.photogallery.common;
+
+public class Constants {
+
+	private Constants() {}
+
+	public static final String PHOTOS_TOPIC_NAME = "photos";
+	public static final String LIKES_TOPIC_NAME = "likes";
+
+}
