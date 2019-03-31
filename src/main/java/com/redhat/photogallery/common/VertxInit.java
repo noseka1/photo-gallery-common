9f4c03b319515c8b82b42dcf1dff85d873e54c16
Ales Nosek <anosek@redhat.com>
2019-03-30 23:24:39 -0700

    Initialize Vert.x at one place

---
+++ src/main/java/com/redhat/photogallery/common/VertxInit.java
@@ -0,0 +1,27 @@
+package com.redhat.photogallery.common;
+
+import io.reactivex.functions.Consumer;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.core.logging.SLF4JLogDelegateFactory;
+import io.vertx.reactivex.core.Vertx;
+
+public class VertxInit {
+
+	private VertxInit() {};
+
+	static {
+		System.setProperty("vertx.logger-delegate-factory-class-name", SLF4JLogDelegateFactory.class.getName());
+	}
+
+	private static final Logger LOG = LoggerFactory.getLogger(VertxInit.class);
+
+	public static void createClusteredVertx(Consumer<Vertx> onSuccess) {
+		Vertx.rxClusteredVertx(new VertxOptions()).subscribe(onSuccess, VertxInit::vertxError);
+	}
+
+	private static void vertxError(Throwable t) {
+		LOG.error("Failed to initialize Vert.x");
+	}
+}
